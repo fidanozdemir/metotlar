e37ff914bf51ed0aa5c9b7faef26f591f278c90f
fidanozdemir <fidannoz@gmail.com>
2021-02-12 22:03:01 +0300

    1

---
+++ metotlar/sepetmanager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metotlar
+{
+    class sepetmanager
+    {
+        public void Ekle()
+        {
+            Console.WriteLine("Tebrikler.Sepete eklendi!");
+        }
+
+    }
+}
