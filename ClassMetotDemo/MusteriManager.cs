bf2ada04c54701f2b14fc526c4b4be8ff5aba513
fidanozdemir <fidannoz@gmail.com>
2021-02-13 15:44:03 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle (Musteri musteri, Musteri musteri1)
+        {
+            Console.WriteLine("Musteri Id: " + musteri.MusteriId + " eklendi!" + musteri1.MusteriId + " eklendi!");
+            //Console.WriteLine("Musteri TcNo: " + musteri.MusteriTcNo);
+            //Console.WriteLine("Musteri Adi: " + musteri.MusteriAdi);
+            //Console.WriteLine("Musteri Soyadi: " + musteri.MusteriSoyadi);
+        }
+
+        public void Listele(Musteri musteri1, Musteri musteri2, Musteri musteri3)
+        {
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach (Musteri musteri in musteriler)
+            {
+
+                Console.WriteLine("Musteri Id: " + musteri.MusteriId);
+                Console.WriteLine("Musteri TcNo: " + musteri.MusteriTcNo);
+                Console.WriteLine("Musteri Adi: " + musteri.MusteriAdi);
+                Console.WriteLine("Musteri Soyadi: " + musteri.MusteriSoyadi);
+            }
+        }
+
+        public void Sil (Musteri musteri,Musteri musteri1)
+        {
+            Console.WriteLine(musteri.MusteriAdi + " silindi!"+ musteri1.MusteriAdi + " silindi!");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriTcNo = "12345678901";
+            musteri1.MusteriAdi = "Eva";
+            musteri1.MusteriSoyadi = "Demir";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 2;
+            musteri2.MusteriTcNo = "12345678902";
+            musteri2.MusteriAdi = "Mehmet";
+            musteri2.MusteriSoyadi = "Yılmaz";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriId = 3;
+            musteri3.MusteriTcNo = "12345678903";
+            musteri3.MusteriAdi = "Deniz";
+            musteri3.MusteriSoyadi = "Kaya";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1,musteri2);
+            musteriManager.Listele(musteri1, musteri2, musteri3);
+            musteriManager.Sil(musteri2,musteri3);
+
+
+
+
+
+
+
+        }
+    }
+}
